cccfeb919ff36a07a6fbffd3a758f1c23dd186b3
JulioAguiar-ux <juliocostadeaguiar@gmail.com>
2026-04-01 03:51:24 -0300

    blog pessoal

---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,16 @@
+import {Module} from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { Postagem } from "./entities/postagem.entity";
+import { PostagemService } from "./services/postagem.service";
+import { PostagemController } from "./controllers/postagem.controllers";
+
+@Module ({
+    imports : [TypeOrmModule.forFeature([Postagem])],
+    providers: [ PostagemService],
+    controllers: [ PostagemController],
+    exports: [],
+
+}) 
+
+export class PostagemModule {}

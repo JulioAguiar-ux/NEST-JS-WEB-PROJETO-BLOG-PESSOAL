a0d4e2f356e8a0d6af81ebf3b59c6e1071482d4c
JulioAguiar-ux <juliocostadeaguiar@gmail.com>
2026-04-02 22:36:41 -0300

    get, post, put, delete

--- src/postagem/controllers/postagem.controllers.ts
+++ src/postagem/controllers/postagem.controllers.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { PostagemService } from './../services/postagem.service';
+import {Body, Controller, Get, HttpCode, HttpStatus, ParseIntPipe, Param, Post, Put, Delete } from "@nestjs/common";
 import { Postagem } from "../entities/postagem.entity";
-import { PostagemService } from "../services/postagem.service";
+
 
 @Controller("/postagens")
 export class PostagemController {
@@ -13,4 +14,35 @@ export class PostagemController {
     return this.postagemService.findAll();
   }
 
+ @Get('/:id')
+@HttpCode(HttpStatus.OK)
+findByID(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+  return this.postagemService.findById(id);
 }
+
+@Get('/titulo/:titulo')
+@HttpCode(HttpStatus.OK)
+findByALLTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+  return this.postagemService.findALLByTitulo(titulo);
+}
+
+@Post()
+@HttpCode(HttpStatus.CREATED)
+create(@Body() postagem: Postagem): Promise<Postagem> {
+  return this.postagemService.create(postagem);
+}
+
+@Put()
+@HttpCode(HttpStatus.OK)
+update(@Body() postagem: Postagem): Promise<Postagem> {
+  return this.postagemService.update(postagem);
+}
+
+@Delete('/:id')
+@HttpCode(HttpStatus.NO_CONTENT)
+delete(@Param('id', ParseIntPipe) id: number){
+    return this.postagemService.delete(id);
+}
+
+};
+
